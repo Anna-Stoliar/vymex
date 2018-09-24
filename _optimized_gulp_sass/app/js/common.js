73253e55c18439bbb1fba0e32e25928577dbb9ff
Anna-Stoliar <Anna.S0Anna.S@gmail.com>
2018-09-24 16:49:31 +0300

    header

--- _optimized_gulp_sass/app/js/common.js
+++ _optimized_gulp_sass/app/js/common.js
@@ -9,19 +9,3 @@ $(".lang-en").click(function() {
 	$('.lang-en').addClass('active');
 });
 
-var w = document.getElementById("logo").offsetWidth;
-
-$(document).ready(function() {
-	if (window.innerWidth < 831) {
-		$(".header-auth").css("margin-left", w);
-	}
-});
-
-$(window).resize(function(){
-	if (window.innerWidth < 831) {
-		$(".header-auth").css("margin-left", w);
-	}
-	if (window.innerWidth > 830) {
-		$(".header-auth").css("margin-left", "0");
-	}
-});
